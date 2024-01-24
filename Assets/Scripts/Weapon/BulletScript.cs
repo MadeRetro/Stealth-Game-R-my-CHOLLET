83436ee8b446b847a2fbead33d5a087275ba0b12
Rémy CHOLLET <153066606+MadeRetro@users.noreply.github.com>
2024-01-24 15:38:16 +0100

    [update]
    Three enemies and boundaries

---
+++ Assets/Scripts/Weapon/BulletScript.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    [SerializeField] private float bulletSpeed;
+    [SerializeField] private AudioSource ASRef;
+    [SerializeField] private GameObject bulletImpact;
+    private Vector3 position;
+    private bool spawnParticles;
+
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, position, bulletSpeed* Time.deltaTime);
+
+        if(transform.position == position)
+        {
+            if(spawnParticles )
+            {
+                Instantiate(bulletImpact, transform.position, transform.rotation);
+            }
+            DestroyBullet();
+        }
+    }
+
+    private void DestroyBullet()
+    {
+        Destroy(gameObject);
+    }
+
+    public void setTargetPos(Vector3 newPosition, bool newSpawnParticles)
+    {
+        position = newPosition;
+        spawnParticles = newSpawnParticles;
+    }
+}
